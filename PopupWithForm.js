99f94aadccffe15f5421118aa61117f8fa419706
Korvin Hofilena <165986465+KorvinHofilena@users.noreply.github.com>
2024-08-05 03:46:05 -0700

    Add files via upload

--- PopupWithForm.js
+++ PopupWithForm.js
@@ -1,13 +1,11 @@
 import Popup from "./Popup.js";
 
-class PopupWithForm extends Popup {
-  constructor(popupSelector, handleFormSubmit) {
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, { handleFormSubmit }) {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
     this._form = this._popup.querySelector(".modal__form");
     this._inputList = this._form.querySelectorAll(".modal__input");
-    this._submitButton = this._form.querySelector(".modal__button");
-    this._submitButtonText = this._submitButton.textContent;
   }
 
   _getInputValues() {
@@ -15,12 +13,13 @@ class PopupWithForm extends Popup {
     this._inputList.forEach((input) => {
       this._formValues[input.name] = input.value;
     });
+
     return this._formValues;
   }
 
   setEventListeners() {
     super.setEventListeners();
-    this._form.addEventListener("submit", (evt) => {
+    this._popup.addEventListener("submit", (evt) => {
       evt.preventDefault();
       this._handleFormSubmit(this._getInputValues());
     });
@@ -31,5 +30,3 @@ class PopupWithForm extends Popup {
     this._form.reset();
   }
 }
-
-export default PopupWithForm;
