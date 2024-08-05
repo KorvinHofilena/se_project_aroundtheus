45b89d4834ce112d9dc1536d5bd36c2e4d97bf35
Korvin Hofilena <165986465+KorvinHofilena@users.noreply.github.com>
2024-08-04 22:08:54 -0700

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -7,45 +7,36 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/",
   },
   mode: "development",
   devServer: {
-    static: path.resolve(__dirname, "dist"),
-    open: true,
-    hot: true,
-    port: 8080,
-    historyApiFallback: true,
-    client: {
-      overlay: true,
-    },
+    contentBase: path.resolve(__dirname, "dist"),
+    compress: true,
+    port: 8081,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
-      favicon: "./src/images/favicon.ico",
     }),
+    new CleanWebpackPlugin(),
   ],
 };
