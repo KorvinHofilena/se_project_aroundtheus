7dd0cf557ddb5f8acaa7e051dbc7be97b4c6a7fb
Korvin Hofilena <165986465+KorvinHofilena@users.noreply.github.com>
2024-08-04 22:10:16 -0700

    Add files via upload

---
+++ PopupWithForm.js
@@ -0,0 +1,35 @@
+import Popup from "./Popup.js";
+
+class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector(".modal__form");
+    this._inputList = this._form.querySelectorAll(".modal__input");
+    this._submitButton = this._form.querySelector(".modal__button");
+    this._submitButtonText = this._submitButton.textContent;
+  }
+
+  _getInputValues() {
+    this._formValues = {};
+    this._inputList.forEach((input) => {
+      this._formValues[input.name] = input.value;
+    });
+    return this._formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+}
+
+export default PopupWithForm;
